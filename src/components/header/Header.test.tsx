89577b03a69b40e4def7acfa613c7668f9a387b8
JJoanka <joanna.jurasz21@gmail.com>
2024-03-27 11:44:00 +0100

    Add header test file

--- src/components/header/Header.test.tsx
+++ src/components/header/Header.test.tsx
@@ -1,22 +1,25 @@
 import { render } from "@testing-library/react";
-import "@testing-library/jest-dom";
 import Header from "./Header";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { ComponentProps } from "react";
 
-vi.mock("../searchBar/SearchBar.tsx", () => ({
-  SearchBar: () => <div>SearchBar</div>,
+vi.mock("@mui/material/Box", () => ({
+  default: (props: ComponentProps<typeof Box>) => <div>{props.children}</div>,
 }));
 
-vi.mock("../menu/Menu.tsx", () => ({
-  Menu: () => <div>Menu</div>,
+vi.mock("@mui/material/Typography", () => ({
+  default: (props: ComponentProps<typeof Typography>) => (
+    <div>{props.children}</div>
+  ),
 }));
 
-describe("LinkToPage", () => {
+describe("Header", () => {
   it("should render the children properly", () => {
     // ARRANGE && ACT
     const { getByText } = render(<Header>Header</Header>);
 
     // ASSERT
-    expect(getByText("SearchBar")).toBeInTheDocument();
-    expect(getByText("Menu")).toBeInTheDocument();
+    expect(getByText("Header")).toBeInTheDocument();
   });
 });
